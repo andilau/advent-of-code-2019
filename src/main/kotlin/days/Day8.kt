6622f1b707f4b8f5a55587ba0bb5de843975fd7a
Andi Lau <andilau@users.noreply.github.com>
2021-11-29 16:26:47 +0100

    Add Day8 "Space Image Format"
    * Day8
    
    * Add input_day_8.txt
    
    * Mod README.md add Day 8
    
    * Mod SolutionsTest
    
    * Add Test
    
    * Mod Day8 constants
    
    * Improve tests
    
    * Minor enhancement

---
+++ src/main/kotlin/days/Day8.kt
@@ -0,0 +1,37 @@
+package days
+
+@AdventOfCodePuzzle(
+    name = "Space Image Format",
+    url = "https://adventofcode.com/2019/day/8",
+    date = Date(day = 8, year = 2019)
+)
+class Day8(input: String) : Puzzle {
+    private val layers = input.chunked(WIDE * TALL)
+
+    override fun partOne() =
+        layers
+            .minByOrNull { layer -> layer.count { it == BLACK } }
+            ?.let { layer -> layer.count { it == WHITE } * layer.count { it == TRANSPARENT } }
+            ?: 0
+
+    override fun partTwo() =
+        layers
+            .map { layer ->
+                layers.first().indices
+                    .map { at -> layer[at] }
+                    .firstOrNull { pixel -> pixel != TRANSPARENT }
+            }
+            .chunked(WIDE)
+            .forEach {
+                println(it.joinToString(""))
+            }
+
+     companion object {
+        var WIDE = 25
+        var TALL = 6
+        const val BLACK = '0'
+        const val WHITE = '1'
+        const val TRANSPARENT = '2'
+    }
+
+}
