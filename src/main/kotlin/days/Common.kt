d837d632ea507ca0e1bfd52fc6655c7f67900642
Andi Lau <andi@herrlau.de>
2022-03-10 14:05:11 +0100

    Add Day11 "Space Police"

--- src/main/kotlin/days/Common.kt
+++ src/main/kotlin/days/Common.kt
@@ -2,6 +2,9 @@ package days
 
 import kotlin.math.absoluteValue
 
+fun String.toLongNumbers(): LongArray =
+    split(",").map(String::toLong).toLongArray()
+
 fun <T> Collection<Iterable<T>>.flattenByIndex(): Sequence<T> = sequence {
     var index = 0
     while (true) {
@@ -25,4 +28,16 @@ data class Point(val x: Int, val y: Int) {
     companion object {
         val ORIGIN = Point(0, 0)
     }
+}
+
+enum class Direction() {
+    NORTH, EAST, SOUTH, WEST;
+
+    fun left() = when (this) {
+        NORTH -> WEST
+        WEST -> SOUTH
+        SOUTH -> EAST
+        EAST -> NORTH
+    }
+    fun right() = left().left().left()
 }
---
+++ src/main/kotlin/days/Day11.kt
@@ -0,0 +1,101 @@
+package days
+
+import days.Day11.PaintRobot.PanelColor
+import days.Day11.PaintRobot.PanelColor.BLACK
+import days.Day11.PaintRobot.PanelColor.WHITE
+import days.Direction.*
+import days.Point.Companion.ORIGIN
+
+@AdventOfCodePuzzle(
+    name = "Space Police",
+    url = "https://adventofcode.com/2019/day/11",
+    date = Date(day = 11, year = 2019)
+)
+class Day11(val input: String) : Puzzle {
+    private val program = input.toLongNumbers()
+
+    override fun partOne(): Int =
+        Panels()
+            .paintWith(CompleteIntCodeComputer(program))
+            .painted()
+
+    override fun partTwo(): Unit =
+        Panels().apply { put(ORIGIN, WHITE) }
+            .paintWith(CompleteIntCodeComputer(program))
+            .asString()
+            .run { print(this) }
+
+    class Panels : MutableMap<Point, PanelColor> by HashMap() {
+        fun painted() = size
+
+        fun paintWith(computer: CompleteIntCodeComputer): Panels {
+            val robot = PaintRobot(computer)
+
+            while (!robot.halted()) {
+                val position = robot.position
+                val currColor = getOrDefault(position, BLACK)
+                val nextColor = robot.step(currColor)
+                this[position] = nextColor
+            }
+            return this
+        }
+
+        fun asString() = buildString {
+            val map = this@Panels
+            for (y in keys.maxOf(Point::y) downTo keys.minOf(Point::y)) {
+                val line = (keys.minOf(Point::x)..keys.maxOf(Point::x)).map { x ->
+                    map.getOrDefault(Point(x, y), BLACK)
+                }.map { if (it == BLACK) ' ' else '#' }.joinToString("")
+                appendLine(line)
+            }
+        }
+    }
+
+    data class PaintRobot(
+        val computer: CompleteIntCodeComputer,
+        var position: Point = ORIGIN,
+        var direction: Direction = NORTH
+    ) {
+
+        fun halted() = computer.halted
+
+        fun step(panelColor: PanelColor): PanelColor {
+            if (computer.halted) throw IllegalStateException("Paint Robot halted.")
+
+            computer
+                .apply { input = panelColor.ordinal.toLong() }
+                .run()
+            val paint = computer.output.toInt()
+            val rightOrLeft = computer.output.toInt()
+
+            when (rightOrLeft) {
+                0 -> turnLeft()
+                1 -> turnRight()
+            }
+            move()
+
+            return PanelColor.from(paint)
+        }
+
+        private fun turnLeft() = direction.left().also { direction = it }
+        private fun turnRight() = direction.right().also { direction = it }
+        private fun move() = when (direction) {
+            NORTH -> position.up()
+            EAST -> position.right()
+            SOUTH -> position.down()
+            WEST -> position.left()
+        }.also { position = it }
+
+        enum class PanelColor {
+            BLACK, WHITE;
+
+            companion object {
+                fun from(index: Int) = when (index) {
+                    0 -> BLACK
+                    1 -> WHITE
+                    else -> throw IllegalArgumentException("Not a color id: $index")
+                }
+            }
+        }
+    }
+}
