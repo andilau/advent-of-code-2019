ed2bbafa5db478d9ffff67dc8fdb5e988f132a7a
Andi Lau <andilau@users.noreply.github.com>
2022-03-14 11:45:49 +0100

    Add Day12 "The N-Body Problem" (#7)

--- src/main/kotlin/days/Common.kt
+++ src/main/kotlin/days/Common.kt
@@ -1,6 +1,7 @@
 package days
 
 import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 fun String.toLongNumbers(): LongArray =
     split(",").map(String::toLong).toLongArray()
@@ -23,7 +24,8 @@ data class Point(val x: Int, val y: Int) {
     fun left() = copy(x = x - 1)
     fun right() = copy(x = x + 1)
 
-    fun manhattanDistance(to: Point = ORIGIN) = (x - to.x).absoluteValue + (y - to.y).absoluteValue
+    fun manhattanDistance(to: Point = ORIGIN) =
+        (x - to.x).absoluteValue + (y - to.y).absoluteValue
 
     companion object {
         val ORIGIN = Point(0, 0)
@@ -40,4 +42,29 @@ enum class Direction() {
         EAST -> NORTH
     }
     fun right() = left().left().left()
+}
+
+data class Point3D(val x: Int, val y: Int, val z: Int) {
+    infix fun sign(other: Point3D) = Point3D(
+        (other.x - x).sign,
+        (other.y - y).sign,
+        (other.z - z).sign,
+    )
+
+    operator fun plus(other: Point3D) = Point3D(x + other.x, y + other.y, z + other.z)
+
+    fun manhattanDistance(to: Point3D = ORIGIN) =
+        (x - to.x).absoluteValue + (y - to.y).absoluteValue + (z - to.z).absoluteValue
+
+    companion object {
+        val ORIGIN = Point3D(0, 0, 0)
+    }
+}
+
+fun lcm(x: Long, y: Long, vararg ints: Long): Long =
+    ints.fold(x * y / gcd(x, y)) { acc, z -> lcm(acc, z) }
+
+fun gcd(a: Long, b: Long): Long {
+    if (b == 0L) return a
+    return gcd(b, a % b)
 }
