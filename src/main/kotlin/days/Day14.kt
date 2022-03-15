b5e7c69dc9c6811767b64cc7b98ba618363aee9a
Andi Lau <andi@herrlau.de>
2022-03-15 15:20:34 +0100

    Add Day14 "Space Stoichiometry" setup

---
+++ src/main/kotlin/days/Day14.kt
@@ -0,0 +1,20 @@
+package days
+
+@AdventOfCodePuzzle(
+    name = "Space Stoichiometry",
+    url = "https://adventofcode.com/2019/day/14",
+    date = Date(day = 14, year = 2019)
+)
+class Day14(val reactions: List<String>) : Puzzle {
+
+    override fun partOne(): Int {
+        return 0
+    }
+
+    override fun partTwo(): Int {
+        return 0
+    }
+
+    companion object {
+    }
+}
