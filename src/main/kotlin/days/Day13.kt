6e4d6891dbe321fe21b8e3d2efff3f9750e007b6
Andi Lau <andi@herrlau.de>
2022-03-15 08:43:10 +0100

    Add Day13 "Care Package"

---
+++ src/main/kotlin/days/Day13.kt
@@ -0,0 +1,58 @@
+package days
+
+import kotlin.math.sign
+
+
+@AdventOfCodePuzzle(
+    name = "Care Package",
+    url = "https://adventofcode.com/2019/day/13",
+    date = Date(day = 13, year = 2019)
+)
+class Day13(val program: IntArray) : Puzzle {
+
+    override fun partOne(): Int = outputAsSequence(program)
+        .windowed(3, 3)
+        .count { it.last() == BLOCK }
+
+    override fun partTwo(): Int {
+        val map = program.map(Int::toLong).toLongArray().apply { this[0] = 2 }
+        val computer = CompleteIntCodeComputer(map)
+
+        var score = 0
+        var paddle = 0
+        while (true) {
+            computer.run()
+            try {
+                val x = computer.output.toInt()
+                computer.output.toInt()
+                val block = computer.output
+
+                when {
+                    (x == -1) -> score = block.toInt()
+                    block == PADDLE -> paddle = x
+                    block == BALL -> computer.input = (x - paddle).sign.toLong()
+                }
+            } catch (e: IllegalStateException) {
+                return score
+            }
+        }
+    }
+
+    private fun outputAsSequence(program: IntArray) = sequence {
+        val computerWithOutput = CompleteIntCodeComputer(program.map(Int::toLong).toLongArray()).run()
+        while (true) {
+            try {
+                yield(computerWithOutput.output)
+            } catch (e: Exception) {
+                break
+            }
+        }
+    }
+
+    companion object {
+        private const val WALL = 1L
+        private const val BLOCK = 2L
+        private const val PADDLE = 3L
+        private const val BALL = 4L
+    }
+}
