5fee5f55997e12105687190c011e581af69a01d6
Andi Lau <andi@herrlau.de>
2022-04-05 20:53:58 +0200

    Add Day17 "Set and Forget" partTwo wip

--- src/main/kotlin/days/Day17.kt
+++ src/main/kotlin/days/Day17.kt
@@ -9,37 +9,52 @@ private const val SCAFFOLD: Char = '#'
 )
 class Day17(input: LongArray) : Puzzle {
     private val computer = CompleteIntCodeComputer(input)
-
-    override fun partOne(): Int {
-        val scaffold = outputfromComputer(computer)
-            .joinToString("")
-            .lines()
-            .flatMapIndexed { row, line ->
-                line.mapIndexedNotNull { column, c -> if (c == SCAFFOLD) Point(row, column) else null }
+    private val scaffold = outputFromComputer(computer)
+        .joinToString("")
+        .lines()
+        .flatMapIndexed { row, line ->
+            line.mapIndexedNotNull { column, c ->
+                if (c == SCAFFOLD || c in "<>^v") Point(column, row) to c
+                else null
             }
-            .toSet()
+        }
+        .toMap()
 
-        return scaffold
+    override fun partOne(): Int =
+        scaffold
+            .keys
             .filter { it.neighbors().all { neighbor -> neighbor in scaffold } }
             .sumOf { it.x * it.y }
-    }
 
     override fun partTwo(): Int {
+        scaffold
+            .mapAsString('.') { it }
+            .also { println(it) }
+/* Manual solution:
+        "R4,R10,R8,R4,"     -> A
+        "R10,R6,R4,"        -> B
+        "R4,R10,R8,R4,"     -> A
+        "R10,R6,R4,"        -> B
+        "R4,L12,R6,L12,"    -> C
+        "R10,R6,R4,"        -> B
+        "R4,L12,R6,L12,"    -> C
+        "R4,R10,R8,R4,"     -> A
+        "R10,R6,R4"         -> B
+        "R4,L12,R6,L12"     -> C
+*/
         return 0
     }
 
-    private fun outputfromComputer(computer: CompleteIntCodeComputer) = sequence {
+    private fun outputFromComputer(computer: CompleteIntCodeComputer) = sequence {
         while (!computer.halted) {
             computer.run(true)
 
             val char = try {
                 computer.output.toInt().toChar()
             } catch (e: Exception) {
-                println(e)
+                // println(e)
             }
-            //println("char = ${char}")
             yield(char)
         }
     }
-
 }
